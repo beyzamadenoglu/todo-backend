a41047d20b2625338874409de46b4647e3efef85
Madenoglu, Beyza, Vodafone (External) <beyza_maden97@hotmail.com>
2023-01-27 19:22:16 +0300

    ignore env

--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DB_CONNECTION_STRING, {
-  // connecting to the mongodb database name: "todo-app" locally
-  keepAlive: true, // keeping the connection alive
+  keepAlive: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-mongoose.set("debug", true) // enabling debugging information to be printed to the console for debugging purposes
+mongoose.set("debug", true)
 mongoose.Promise = Promise 
 
 module.exports.Todo = require("./todo") // requiring the todo model that we just created in mongodb
